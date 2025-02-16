ac71a4742412018387f577230259b4a36fd0d1a1
Philip Ogboh <philipogboh60@gmail.com>
2025-02-16 23:31:22 +0100

    update

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { RequestHandler } from "express";
 import Admin from "../models/Admin";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
@@ -22,6 +22,43 @@ export const login: RequestHandler = async (req, res, next) => {
 
     res.json({ token });
   } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const register: RequestHandler = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Validate input
+  if (!username || !password) {
+    res.status(400).json({ message: "Username and password are required." });
+    return;
+  }
+
+  try {
+    // Check if an admin with the given username already exists
+    const existingAdmin = await Admin.findOne({ username });
+    if (existingAdmin) {
+      res.status(400).json({ message: "Admin already exists" });
+      return;
+    }
+
+    // Generate salt and hash the password
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create and save the new admin
+    const admin = new Admin({
+      username,
+      password: hashedPassword,
+    });
+
+    await admin.save();
+
+    res.status(201).json({ message: "Admin created successfully" });
+  } catch (error) {
+    console.error("Error creating admin:", error);
     res.status(500).json({ message: "Server error" });
   }
 };
